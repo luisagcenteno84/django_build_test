79eeeaec11616211495f4c11358b61d334e96a73
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-07 19:37:56 -0700

    fixing hello_world

--- django_build_test/urls.py
+++ django_build_test/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello_world/'<name>', views.hello_world),
+    path('hello_world/name', views.hello_world),
 ]
