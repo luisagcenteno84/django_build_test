06cb732a09aafc476f13af7ed1b987d2c570376d
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-07 19:52:59 -0700

    corrected views

---
+++ django_build_test/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def hello_world(request):
+ 
+    return '<h1>Hello World!!</h1>'
--- django_build_test/views/hello_world.py
+++
@@ -1,6 +0,0 @@
-class Hello_world():
-    def __init__(self):
-        self.name = "Hello World!"
-
-    def get_name(self):
-        return self.name
